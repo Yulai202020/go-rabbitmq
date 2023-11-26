92ab9ea45ee62f9ce85554d01362ac1678b1980b
Yulai202020 <ngmyulai@gmail.com>
2023-11-26 21:25:48 +0500

    first commit

--- out.go
+++ out.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"database/sql"
 	"encoding/json"
 	"github.com/joho/godotenv"
 	"github.com/streadway/amqp"
+	_ "github.com/lib/pq" // <------------ here
 )
 
-func JsonParser(jsonData []byte) map[string]interface{} {
-	var data map[string]interface{}
+func JsonParser(jsonData []byte) []map[string]interface{} {
+	var data []map[string]interface{}
+	
+	
 
-	err := json.Unmarshal(jsonData, &data)	
+	err := json.Unmarshal(jsonData, &data)
 	failOnError(err, "")
+	
+	resultMap := make(map[string]interface{})
+
+	for _, item := range data {
+		for key, value := range item {
+			resultMap[key] = value
+		}
+	}
 
 	return data
 }
@@ -26,15 +35,16 @@ func main(){
 	err := godotenv.Load()
 	failOnError(err, "load")
 
-	var dbname = os.Getenv("dbname")
-	var host = os.Getenv("host")
-	var port = os.Getenv("port")
-	var user = os.Getenv("user")
-	var password = os.Getenv("password")
+	// var dbname = os.Getenv("dbname")
+	// var host = os.Getenv("host")
+	// var port = os.Getenv("port")
+	// var user = os.Getenv("user")
+	// var password = os.Getenv("password")
+	// var 
 	var queueName = "Test"
 
 	// Postgres info
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	// Reading from RabbitMQ
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
@@ -72,28 +82,34 @@ func main(){
 
 	failOnError(err, "Failed to register a consumer.")
 
-	db, err := sql.Open("postgres", psqlInfo)
+	// db, err := sql.Open("postgres", psqlInfo)
 
 	failOnError(err, "Cant connect to PostgreSQL.")
 
-  	defer db.Close()
+  	// defer db.Close()
 
+	// err = db.Ping()
+
+	failOnError(err, "Cant find sql server.")
+	// sqlStatement := `INSERT INTO emp (ename, sal, email)`
 	// Writting data to Postgres
-	msgs := JsonParser(msgs.Body)
 	
 	go func() {
 		for msg := range msgs {
-			message := string(msg.Body)
-
-			_, err = db.Exec("INSERT INTO $1($2) VALUES($3)", db.tablename, db.column_name, message)
-			failOnError(err, "Error inserting into PostgreSQL.")
+			message := JsonParser(msg.Body)
+			for k,v := range message {
+				fmt.Println(k)
+				for k1,v1 := range v {
+					fmt.Println(k1,":",v1)
+				}
+				failOnError(err, "Error inserting into PostgreSQL.")
+			}
 		}
 	}()
 }
 
-
 func failOnError(err error, msg string) {
 	if err != nil {
-	  	fmt.Println("%s: %s", msg, err)
+	  	fmt.Printf("%s: %s", msg, err)
 	}
-}	
+}
